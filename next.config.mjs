4cb832f55576bc4a99b2535d666cbf415ac6d5f0
Lucas-Delacroix <delacroixdev@gmail.com>
2026-05-21 23:27:42 -0300

    fix: multiplayer wss url

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  env: {
-    NEXT_PUBLIC_PARTYKIT_HOST:
-      process.env.NEXT_PUBLIC_PARTYKIT_HOST || "127.0.0.1:1999",
-  },
 };
 
 export default nextConfig;
